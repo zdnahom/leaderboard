45177a89556f46361c1a39193cadd271ab2266b0
Nahom <zerihunnahom@gmail.com>
2023-03-07 19:01:21 +0300

    Fix linter

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 import './style.css';
 
-const scores=[100,20,50,78,12,77,42]
-const leaders=document.querySelector('.leaders')
+const scores = [100, 20, 50, 78, 12, 77, 42];
+const leaders = document.querySelector('.leaders');
 
 const generateScores = (data) => {
-    data.forEach((item) => {
-      const player = document.createElement('div');
-      player.innerHTML = `
+  data.forEach((item) => {
+    const player = document.createElement('div');
+    player.innerHTML = `
           <p> Name: ${item}</p>
           `;
-      leaders.appendChild(player);
-    });
-  };
+    leaders.appendChild(player);
+  });
+};
 
-  generateScores(scores)
+generateScores(scores);
