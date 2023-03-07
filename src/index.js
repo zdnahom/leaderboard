3ff8ce67567c9d744c7daef0435b2428968aaf6b
Nahom <zerihunnahom@gmail.com>
2023-03-07 18:56:35 +0300

    Modify index.js to dynamically create leaderboard

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
 import './style.css';
+
+const scores=[100,20,50,78,12,77,42]
+const leaders=document.querySelector('.leaders')
+
+const generateScores = (data) => {
+    data.forEach((item) => {
+      const player = document.createElement('div');
+      player.innerHTML = `
+          <p> Name: ${item}</p>
+          `;
+      leaders.appendChild(player);
+    });
+  };
+
+  generateScores(scores)
