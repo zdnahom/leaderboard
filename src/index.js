f93170ae2d193a12009504aaac87ec98ba93ef70
Nahom <zerihunnahom@gmail.com>
2023-03-08 15:50:22 +0300

    Create new game

--- src/index.js
+++ src/index.js
@@ -1,16 +1,35 @@
-import './style.css';
+import "./style.css";
+
 
 const scores = [100, 20, 50, 78, 12, 77, 42];
-const leaders = document.querySelector('.leaders');
+const leaders = document.querySelector(".leaders");
 
 const generateScores = (data) => {
   data.forEach((item) => {
-    const player = document.createElement('div');
+    const player = document.createElement("div");
     player.innerHTML = `
           <p> Name: ${item}</p>
           `;
     leaders.appendChild(player);
   });
 };
-
+let id;
+const createNewGame = async () => {
+  const res = await fetch(
+    "https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/",
+    {
+      method: "POST",
+      body: JSON.stringify({
+        name: "My cool new game",
+      }),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      }
+    }
+  )
+  const data=await res.json()
+  id=data.result.replace(/(Game with ID:)|(added.)|(\W)/g,'')
+};
+createNewGame()
 generateScores(scores);
+
