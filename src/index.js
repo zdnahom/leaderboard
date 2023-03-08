316f9212d2963f224ac164d17f9834c7db6f5a67
Nahom <zerihunnahom@gmail.com>
2023-03-08 16:31:51 +0300

    Add refreshBoard function

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import "./style.css";
 
-
 const scores = [100, 20, 50, 78, 12, 77, 42];
 const leaders = document.querySelector(".leaders");
 
@@ -8,28 +7,35 @@ const generateScores = (data) => {
   data.forEach((item) => {
     const player = document.createElement("div");
     player.innerHTML = `
-          <p> Name: ${item}</p>
+          <p> Name: ${item.user}</p>
           `;
     leaders.appendChild(player);
   });
 };
-let id;
+
 const createNewGame = async () => {
   const res = await fetch(
     "https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/",
     {
       method: "POST",
       body: JSON.stringify({
-        name: "My cool new game",
+        name: "Beat Nahom's score",
       }),
       headers: {
         "Content-type": "application/json; charset=UTF-8",
-      }
+      },
     }
-  )
-  const data=await res.json()
-  id=data.result.replace(/(Game with ID:)|(added.)|(\W)/g,'')
+  );
+  const data = await res.json();
+  const id = await data.result.replace(/(Game with ID:)|(added.)|(\W)/g, "");
+  // console.log(id)
+  return id
 };
-createNewGame()
-generateScores(scores);
 
+const refreshBoard = async () => {
+  const res = await fetch(`https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/BwlgxYvt8p5JJSu0N3Ht/scores`);
+  const data=await res.json()
+  generateScores(data);
+};
+
+refreshBoard()
